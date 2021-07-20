6c32791ad285fa41b88e5aa655ab24b5ab17e373
ana colell <anacolell8@gmail.com>
2021-07-20 21:01:01 +0300

    Fetch all info from apis

--- src/index.js
+++ src/index.js
@@ -6,47 +6,45 @@ const reposContainer = document.querySelector('.repos');
 const repoInfo = document.querySelector('.repo-info');
 const backBtn = document.querySelector('.view-repos');
 const filterInput = document.querySelector('.filter-repos');
-const username = 'anacolell';
-/* fetch users */
-
- const fetchUsers = async () => {
-   const userUrl = await fetch(`https://api.github.com/users/${username}`);
-   const userData = await userUrl.json();
-   displayUser(userData)
- }
-
-   const displayUser = (userData) => {
-     let userDiv = document.createElement('div');
-     userDiv.innerHTML = `
-     <img class="avatar" src= ${userData.avatar_url}/>
-     <div class="user-data">
-       <p><strong>Name:</strong> ${userData.name}</p>
-       <p><strong>Location:</strong> ${userData.location}</p>
-       <p><strong>Number of public repos:</strong> ${userData.public_repos}</p>
-     </div>`;
-     userDetails.append(userDiv);
-   }
-
-   fetchUsers()
-
-/* fetch repos from Graphql */
-
-const baseUrl = "https://api.github.com/graphql";
 
 const github_data = {
   "token": process.env.MY_TOKEN,
   "username": "anacolell"
 };
 
+let repos = [];
+
+const fetchUsers = async () => {
+ const userUrl = await fetch(`https://api.github.com/users/${github_data["username"]}`);
+ const userData = await userUrl.json();
+ displayUser(userData)
+}
+
+const displayUser = (userData) => {
+  let userDiv = document.createElement('div');
+  userDiv.innerHTML = `
+    <img class="avatar" src= ${userData.avatar_url}/>
+    <div class="user-data">
+      <p><strong>Name:</strong> ${userData.name}</p>
+      <p><strong>Location:</strong> ${userData.location}</p>
+      <p><strong>Number of public repos:</strong> ${userData.public_repos}</p>
+    </div>`;
+  userDetails.append(userDiv);
+}
+
+fetchUsers()
+
+const baseUrl = "https://api.github.com/graphql";
+
 const headers = {
   'Content-Type': "application/json",
-  'Authorization': "bearer " + github_data["token"]
+  'Authorization': `bearer ${github_data["token"]}`
 }
 
 const body = {
   "query": `
   query {
-    repositoryOwner(login:"anacolell"){
+    repositoryOwner(login:"${github_data["username"]}"){
       repositories(orderBy:{
         field:CREATED_AT, direction:DESC}, first:100, privacy:PUBLIC){
         nodes{
@@ -54,9 +52,10 @@ const body = {
           name,
           description,
           url,
+          homepageUrl,
           languages(first:10){
             nodes{
-                name
+              name
             }
           }
         }
@@ -73,63 +72,82 @@ const fetchRepos = async () => {
   body: JSON.stringify(body)
 })
   let reposResponse = await reposUrl.json();
-  let repos = await reposResponse.data.repositoryOwner.repositories.nodes;
-  console.log(repos)
-
-  filterInput.classList.remove('hide');
-  repos.forEach((repo)=> {
-
-  const repoLanguages = repo.languages.nodes;
-  const languages = document.createElement("ul");
-  languages.classList.add("language-pill")
-
-  repoLanguages.forEach(language => {
-    const lang = document.createElement("li");
-    lang.textContent = Object.values(language)
-    languages.append(lang)
-  })
-      console.log(languages)
-  let reposLi = document.createElement('li');
-  reposLi.classList.add('repo');
-  reposLi.innerHTML = `
-  <h3>${repo.name}</h3>
-  <img src=${repo.openGraphImageUrl}>
-  ${languages.outerHTML}
-  `;
-  repoList.append(reposLi);
-  reposLi.classList.add('repo');
-})
+  repos = await reposResponse.data.repositoryOwner.repositories.nodes;
+  displayRepos(repos)
 }
 
 fetchRepos()
 
-/* fetch individual repos when clicked */
+const displayRepos = (repos) => {
+  filterInput.classList.remove('hide');
+  repos.forEach((repo)=> {
+    const repoLanguages = repo.languages.nodes;
+    const languages = document.createElement("ul");
+    languages.classList.add("language")
+
+    repoLanguages.forEach(language => {
+      const lang = document.createElement("li");
+      lang.textContent = Object.values(language)
+      languages.append(lang)
+    })
+
+    let reposLi = document.createElement('li');
+    reposLi.classList.add('repo');
+    reposLi.innerHTML = `
+      <h3>${repo.name}</h3>
+      <img src=${repo.openGraphImageUrl}>
+      ${languages.outerHTML}
+      <p>${repo.description}</p>
+    `;
+    repoList.append(reposLi);
+    reposLi.classList.add('repo');
+  })
+}
 
 repoList.addEventListener('click', function(e) {
-  if (e.target.matches("h3")){
-    let repoName = e.target.innerText;
-    fetchRepo(repoName)
+  let item = e.target;
+  if (item.classList.contains('repo-list')){
+    return;
+  } else {
+    while (!item.classList.contains('repo')){
+      item = item.parentElement
+    }
   }
+  let repoName = item.querySelector("h3").textContent
+  let repoInfo = {};
+  fetchRepo(repoName)
 })
 
 const fetchRepo = async (repoName) => {
-  const repoUrl = await fetch(`https://api.github.com/repos/${username}/${repoName}`);
-  const repoData = await repoUrl.json();
-  const languagesUrl = await fetch(`https://api.github.com/repos/${username}/${repoName}/languages`);
-  const languagesData = await languagesUrl.json();
-  const languages = Object.keys(languagesData);
-  console.log(repoData)
-  displayRepo(repoData, languages)
+  let repoPic = "";
+  let repoHomeUrl = "";
+  let repoUrl = "";
+  let languagesList = [];
+  let repoDescription = "";
+  repos.forEach((repo) => {
+    if (repo.name === repoName){
+      repoPic = repo.openGraphImageUrl;
+      repoHomeUrl = repo.homepageUrl;
+      repoUrl = repo.url;
+      repoDescription = repo.description
+      repo.languages.nodes.forEach((language) => {
+        languagesList.push(language.name)
+      })
+    }
+  })
+  fetchReadme(repoName)
+  displayRepo(repoPic, languagesList, repoDescription, repoUrl, repoHomeUrl, repoName)
 }
 
-const displayRepo = (repoData, languages) => {
+const displayRepo = (repoPic, languagesList, repoDescription, repoUrl, repoHomeUrl, repoName) => {
   let repoDiv = document.createElement('div');
   repoInfo.innerHTML = "";
   repoDiv.innerHTML = `
-  <p class="repo-name">Name: ${repoData.name}<p>
-  <p>Description: ${repoData.description}<p>
-  <p>Languages: ${languages.join(', ')}
-  <a class="visit" href="${repoData.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub</a>`;
+  <p class="repo-name">Name: ${repoName}<p>
+  <p>Description: ${repoDescription}<p>
+  <p>Languages: ${languagesList}
+  <a class="visit" href="${repoUrl}" target="_blank" rel="noreferrer noopener">View Repo on GitHub</a>
+  <a class="visit" href="${repoHomeUrl}" target="_blank" rel="noreferrer noopener">View live</a>`;
   repoInfo.append(repoDiv);
   reposContainer.classList.add('hide');
   repoInfo.classList.remove('hide');
@@ -137,16 +155,53 @@ const displayRepo = (repoData, languages) => {
   filterInput.classList.add('hide');
 }
 
-/* hide button when going back to repo list */
+const fetchReadme = async (repoName) => {
+  const readmeQuery = {
+  "query": `
+  query {
+    repository(owner: "${github_data["username"]}", name: "${repoName}") {
+    object(expression: "master:README.md") {
+      ... on Blob {
+        text
+      }
+    }
+    }
+  }
+    `
+  }
+  const readmeUrl = await fetch(baseUrl, {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(readmeQuery)
+  })
 
-backBtn.addEventListener('click', (e)=> {
+  let readmeResponse = await readmeUrl.json();
+  let readme = await readmeResponse.data.repository.object.text
+  fetchMarkDown(readme)
+}
+
+ const fetchMarkDown = async (readme) => {
+  const res = await fetch("https://api.github.com/markdown", {
+    method: "POST",
+    headers: {
+      'Accept': 'application/vnd.github.v3+json'
+    },
+    body: JSON.stringify({
+      'text':readme
+    })
+  });
+  let finalReadme = await res.text()
+  const newDiv = document.createElement("div")
+  newDiv.innerHTML = finalReadme
+  repoInfo.append(newDiv)
+ }
+
+backBtn.addEventListener('click', (e) => {
   reposContainer.classList.remove('hide');
   repoInfo.classList.add('hide');
   backBtn.classList.add('hide');
   filterInput.classList.remove('hide')
 })
-
-/* search function */
 
 filterInput.addEventListener('input', (e) => {
   let value = e.target.value;
@@ -161,29 +216,4 @@ filterInput.addEventListener('input', (e) => {
     }
   })
 })
-
-// const fetchReadme = async () => {
-//   const readmeUrl = await fetch(baseUrl, {
-//   method: "POST",
-//   headers: headers,
-//   body: JSON.stringify(bodyQuery)
-// })
-//   let readmeResponse = await readmeUrl.json();
-//   let readme = await readmeResponse.data.repository.object.text
-//   console.log(readme)
-// }
-//    const bodyQuery = {
-//   "query": `
-//   query {
-//     repository(owner: "anacolell", name: "locally") {
-//     object(expression: "master:README.md") {
-//       ... on Blob {
-//         text
-//       }
-//   }
-//   }
-// }
-//   `
-// }
-
 
